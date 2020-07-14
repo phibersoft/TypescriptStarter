9a594b12634f14ce98613fb85e3fe5827b968680
Adem <spirnet@hotmail.com>
2020-07-14 23:22:02 +0300

    First Commit

---
+++ server.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Application } from "express";
+const express = require("express");
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(PORT, () => console.log(`Server up on localhost:${PORT}`));
